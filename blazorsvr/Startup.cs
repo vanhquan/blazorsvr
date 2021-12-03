78c71d5c2aba6acc8df6a486a07b0fa9dfee728d
vanhquan <lanscape@yahoo.com>
2021-12-03 11:27:42 +0700

    1111

--- blazorsvr/Startup.cs
+++ blazorsvr/Startup.cs
@@ -20,6 +20,13 @@ namespace blazorsvr
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            ////
+            //////
+            ///
+
+
+
+
         }
 
         public IConfiguration Configuration { get; }
